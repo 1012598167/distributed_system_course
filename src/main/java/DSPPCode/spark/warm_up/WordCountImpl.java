fbf96400d9c7521ebcb78a61a46b36a042b8dd3d
mathskiller <chennuo909@163.com>
2019-10-26 19:32:56 +0800

    v

--- src/main/java/DSPPCode/spark/warm_up/WordCountImpl.java
+++ src/main/java/DSPPCode/spark/warm_up/WordCountImpl.java
@@ -12,10 +12,12 @@ import java.util.Arrays;
 public class WordCountImpl extends WordCount {
 
     public JavaPairRDD<String, Integer> wordcount(JavaRDD<String> lines) {
+
+        System.out.println(lines.collect().toString());
         return lines
-                .flatMap((String line) -> Arrays.asList(line.split(" ")).iterator())
-                .mapToPair((String word) -> new Tuple2<>(word, 1))
-                .reduceByKey((Integer integer, Integer integer2) -> integer + integer2);
+                .flatMap((String line) -> Arrays.asList(line.split(" ")).iterator())//把其中一个元素按这个规则（生成一列中每个单词的迭代器）映射
+                .mapToPair((String word) -> new Tuple2<>(word, 1))//把映射完的结果组成Tuple
+                .reduceByKey((Integer integer, Integer integer2) -> integer + integer2);//reduce过程中把能加的统统加起来
     }
 
 }
